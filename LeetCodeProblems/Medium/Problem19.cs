69e6c12adac58cf80b4448bf53756ad62edb645f
Sergei Grozov <grozov.sv@gmail.com>
2023-12-18 12:56:11 +0300

    leetcode problem 19

---
+++ LeetCodeProblems/Medium/Problem19.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/remove-nth-node-from-end-of-list/">Remove Nth Node From End of List</see>
+    /// <br>Linked List, Two Pointers</br>
+    /// </summary>
+    internal class Problem19
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode dummy = new(0, head);
+            var slow = dummy;
+            var fast = dummy;
+            var counter = -1;
+
+            while (fast != null)
+            {
+                if (counter < n)
+                {
+                    fast = fast?.next;
+                    counter++;
+                }
+                else
+                {
+                    slow = slow.next;
+                    fast = fast?.next;
+                }
+            }
+
+            slow.next = slow.next.next;
+
+            return dummy.next;
+        }
+    }
+}
