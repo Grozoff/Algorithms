020f7bacd87f961f0423a4e59aeea37615ffe675
Sergei Grozov <grozov.sv@gmail.com>
2023-12-18 12:54:38 +0300

    leetcode problem 237

---
+++ LeetCodeProblems/Medium/Problem237.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeProblems.Medium
+{
+    /**
+    * Definition for singly-linked list.
+    * public class ListNode {
+    *     public int val;
+    *     public ListNode next;
+    *     public ListNode(int x) { val = x; }
+    * }
+    */
+
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/delete-node-in-a-linked-list/">Delete Node in a Linked List</see>
+    /// <br>Linked List</br>
+    /// </summary>
+    internal class Problem237
+    {
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
