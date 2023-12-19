31b583d0c8cd97e25068f362759dc7a8468d1199
Sergei Grozov <grozov.sv@gmail.com>
2023-12-19 21:33:11 +0300

    leetcode problem 234

---
+++ LeetCodeProblems/Easy/Problem234.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/palindrome-linked-list/">Palindrome Linked List</see>
+    /// <br>Linked List, Two Pointers, Stack, Recursion</br>
+    /// </summary>
+    internal class Problem234
+    {
+        public bool IsPalindrome(ListNode head)
+        {
+            var current = head;
+            var lst = new Stack<int>();
+
+            while (head?.next != null)
+            {
+                lst.Push(head.next.val);
+                head = head.next;
+            }
+
+            while (lst.Count > 0)
+            {
+                if (lst.Pop() != current.val)
+                {
+                    return false;
+                }
+
+                current = current.next;
+            }
+
+            return true;
+        }
+    }
+}
