f033ab67fc9d18e6deb3aeb55bf016dc8d6e725b
Sergei Grozov <grozov.sv@gmail.com>
2023-12-23 15:38:36 +0300

    leetcode problem 141

---
+++ LeetCodeProblems/Easy/Problem141.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/linked-list-cycle/">Linked List Cycle</see>
+    /// <br>Hash Table, Linked List, Two Pointers</br>
+    /// </summary>
+    internal class Problem141
+    {
+        public bool HasCycle(ListNode head)
+        {
+            var fast = head;
+            var slow = head;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
