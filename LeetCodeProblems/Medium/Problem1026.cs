8dc63fdfdf1ff4ba9a6bffee3db1847b81c61b26
Sergei Grozov <grozov.sv@gmail.com>
2024-01-11 11:58:52 +0300

    Leetcode problem 1026

---
+++ LeetCodeProblems/Medium/Problem1026.cs
@@ -0,0 +1,32 @@
+﻿using LeetCodeProblems.Easy;
+
+namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/'>Maximum Difference Between Node and Ancestor</see>
+    /// <br>Tree, Depth-First Search, Binary Tree</br>
+    /// </summary>
+    internal class Problem1026
+    {
+        public int MaxAncestorDiff(TreeNode root)
+        {
+            return MaxAncestorDiffHelper(root, int.MaxValue, int.MinValue);
+        }
+
+        private int MaxAncestorDiffHelper(TreeNode node, int minSoFar, int maxSoFar)
+        {
+            if (node == null)
+            {
+                return maxSoFar - minSoFar;
+            }
+
+            int newMin = Math.Min(minSoFar, node.val);
+            int newMax = Math.Max(maxSoFar, node.val);
+
+            int leftDiff = MaxAncestorDiffHelper(node.left, newMin, newMax);
+            int rightDiff = MaxAncestorDiffHelper(node.right, newMin, newMax);
+
+            return Math.Max(leftDiff, rightDiff);
+        }
+    }
+}
