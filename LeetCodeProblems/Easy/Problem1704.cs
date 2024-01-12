c8635bfec8906de01e798ccbe5bb0b9165959b96
Sergei Grozov <grozov.sv@gmail.com>
2024-01-12 14:00:39 +0300

    leetcode problem 1704

---
+++ LeetCodeProblems/Easy/Problem1704.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/determine-if-string-halves-are-alike">Determine if String Halves Are Alike</see>
+    /// <br>String, Counting</br>
+    /// </summary>
+    internal class Problem1704
+    {
+        public bool HalvesAreAlike(string s)
+        {
+            var vowels = new[] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+            return s[..((s.Length + 1) / 2)].Count(c => vowels.Contains(c)) ==
+            s[((s.Length + 1) / 2)..].Count(c => vowels.Contains(c));
+        }
+    }
+}
