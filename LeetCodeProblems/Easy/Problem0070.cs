e92a3e684e2e0d41093cfc21acbe84e4b85a045a
Sergei Grozov <grozov.sv@gmail.com>
2024-01-18 08:48:25 +0300

    leetcode problem 70

---
+++ LeetCodeProblems/Easy/Problem0070.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/climbing-stairs'>Climbing Stairs</see>
+    /// <br>Math, Dynamic Programming, Memoization</br>
+    /// </summary>
+    internal class Problem0070
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n <= 1) return 1;
+
+            int prev = 0;
+            int next = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int sum = prev + next;
+                prev = next;
+                next = sum;
+            }
+            return next;
+        }
+    }
+}
