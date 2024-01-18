2ed0aceef583f4c6e84476cec0b1b3ae454d324d
Sergei Grozov <grozov.sv@gmail.com>
2024-01-18 11:37:43 +0300

    leetcode problem 88

---
+++ LeetCodeProblems/Easy/Problem0088.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/merge-sorted-array/'>Merge Sorted Array</see>
+    /// <br>Array, Two Pointers, Sorting</br>
+    /// </summary>
+    internal class Problem0088
+    {
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                nums1[m + i] = nums2[i];
+            }
+            Array.Sort(nums1);
+        }
+    }
+    public class Tests0088
+    {
+        [Test]
+        public void Test1()
+        {
+
+            int[] nums1 = [1, 2, 3, 0, 0, 0];
+            var m = 3;
+            int[] nums2 = [2, 5, 6];
+            var n = 3;
+            Problem0088.Merge(nums1, m, nums2, n);
+        }
+    }
+}
