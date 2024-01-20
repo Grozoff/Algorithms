0156737c488fd06bc693053b53b988a2d70c5503
Sergei Grozov <grozov.sv@gmail.com>
2024-01-20 12:03:45 +0300

    leetcode problem 191

---
+++ LeetCodeProblems/Easy/Problem0191.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    internal class Problem0191
+    {
+        public static int HammingWeight(int n)
+        {
+            return Convert.ToString(n, 2).Count(x => x == '1');
+            
+            // not my solution
+            int ones = 0;
+            while (n > 0)
+            {
+                ones += (int)(n & 1);
+                n >>= 1;
+            }
+
+            return ones;
+        }
+    }
+}
