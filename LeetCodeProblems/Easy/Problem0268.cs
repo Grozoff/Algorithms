151808005caaf335a0eafcd8aafbb63c9a0162e2
Sergei Grozov <grozov.sv@gmail.com>
2024-01-22 15:31:22 +0300

    leetcode problem 268

---
+++ LeetCodeProblems/Easy/Problem0268.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    internal class Problem0268
+    {
+        public static int MissingNumber(int[] nums)
+        {
+            return Math.Abs(((nums.Length * (nums.Length + 1)) / 2) - nums.Sum());
+
+            // not my solution
+            int x = nums.Length;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                x = x ^ i ^ nums[i];
+            }
+
+            return x;
+        }
+    }
+
+    public class Tests0268
+    {
+        [Test]
+        public void Test1()
+        {
+            int[] input = [3, 0, 1];
+            var output = Problem0268.MissingNumber(input);
+            Assert.That(output, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int[] input = [0, 1];
+            var output = Problem0268.MissingNumber(input);
+            Assert.That(output, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            int[] input = [9, 6, 4, 2, 3, 5, 7, 0, 1];
+            var output = Problem0268.MissingNumber(input);
+            Assert.That(output, Is.EqualTo(8));
+        }
+    }
+}
