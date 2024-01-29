42541b023477ee43e04fb289f95252a867873682
Sergei Grozov <grozov.sv@gmail.com>
2024-01-29 14:15:53 +0300

    leetcode problem 13

---
+++ LeetCodeProblems/Easy/Problem0013.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    internal class Problem0013
+    {
+        public int RomanToInt(string s)
+        {
+            Dictionary<char, int> romanDigits = new()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+            var result = 0;
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                if (romanDigits[s[i]] < romanDigits[s[i + 1]])
+                    result -= romanDigits[s[i]];
+                else
+                    result += romanDigits[s[i]];
+            }
+
+            return result + romanDigits[s[^1]];
+        }
+    }
+}
