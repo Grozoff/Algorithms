607b6e575d9dcdb6a1b9fa5def8105e5402789cb
Sergei Grozov <grozov.sv@gmail.com>
2024-01-29 11:44:02 +0300

    leetcode problem 155

---
+++ LeetCodeProblems/Medium/Problem0155.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/min-stack">Min Stack</see>
+    /// <br>Stack, Design</br>
+    /// </summary>
+    public class MinStack
+    {
+        private Stack<int> _stack = new Stack<int>();
+
+        public MinStack()
+        {
+        }
+
+        public void Push(int val)
+        {
+            _stack.Push(val);
+        }
+
+        public void Pop()
+        {
+            _stack.Pop();
+        }
+
+        public int Top()
+        {
+            return _stack.Peek();
+        }
+
+        public int GetMin()
+        {
+            return _stack.Min();
+        }
+    }
+
+    /**
+     * Your MinStack object will be instantiated and called as such:
+     * MinStack obj = new MinStack();
+     * obj.Push(val);
+     * obj.Pop();
+     * int param_3 = obj.Top();
+     * int param_4 = obj.GetMin();
+     */
+
+    internal class Problem0155
+    {
+    }
+}
