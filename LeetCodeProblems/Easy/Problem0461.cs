0e209f583a8335438d207f1b64a0dec903e30e3c
Sergei Grozov <grozov.sv@gmail.com>
2024-01-29 14:27:56 +0300

    leetcode problem 461

---
+++ LeetCodeProblems/Easy/Problem0461.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/hamming-distance/">Hamming Distance</see>
+    /// <br>Bit Manipulation</br>
+    /// </summary>
+    internal class Problem0461
+    {
+        public int HammingDistance(int x, int y)
+        {
+            return Convert.ToString(x ^ y, 2).Count(x => x == '1');
+        }
+    }
+}
