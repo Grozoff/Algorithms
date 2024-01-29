df479fe2529777475f9c4a5eff46df808e365c3f
Sergei Grozov <grozov.sv@gmail.com>
2024-01-29 14:39:02 +0300

    leetcode problem 190

---
+++ LeetCodeProblems/Easy/Problem0190.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/reverse-bits/">Reverse Bits</see>
+    /// <br>Divide and Conquer, Bit Manipulation</br>
+    /// </summary>
+    internal class Problem0190
+    {
+        public uint reverseBits(uint n)
+        {
+            var bits = Convert.ToString(n, 2).PadLeft(32, '0').ToCharArray();
+            Array.Reverse(bits);
+
+            return Convert.ToUInt32(new string(bits), 2);
+        }
+    }
+}
