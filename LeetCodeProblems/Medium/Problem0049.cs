6cedd6f022ca6c4df9484b7838d031f742892d80
Sergei Grozov <grozov.sv@gmail.com>
2024-02-06 09:28:12 +0300

    leetcode problem 49

---
+++ LeetCodeProblems/Medium/Problem0049.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/group-anagrams">Group Anagrams</see>
+    /// <br>Array, Hash Table, String, Sorting</br>
+    /// </summary>
+    internal class Problem0049
+    {
+        public static IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            var d = new Dictionary<string, IList<string>>();
+
+            foreach (var item in strs)
+            {
+                var t = item.ToArray();
+                Array.Sort(t);
+                var ts = new string(t);
+                if (d.TryGetValue(ts, out IList<string>? value))
+                {
+                    value.Add(item);
+                }
+                else
+                {
+                    d.Add(ts, new List<string>
+                    {
+                        item
+                    });
+                }
+            }
+
+            return d.Values.ToList();
+
+            // linq only
+            return strs
+                    .GroupBy(s => new string(s.OrderBy(c => c).ToArray()))
+                    .Select(g => g.ToList() as IList<string>)
+                    .ToList();
+        }
+    }
+
+    public class Tests49
+    {
+
+        [Test]
+        public void Test1()
+        {
+            var input = new string[]
+            {
+                "eat","tea","tan","ate","nat","bat"
+            };
+
+            var asd = Problem0049.GroupAnagrams(input);
+        }
+    }
+}
