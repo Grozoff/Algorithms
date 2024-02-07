6b7550e16f0bc90d938e9de1e726898b3d9cfbae
Sergei Grozov <grozov.sv@gmail.com>
2024-02-07 10:28:13 +0300

    leetcode problem 451

---
+++ LeetCodeProblems/Medium/Problem0451.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/sort-characters-by-frequency">Sort Characters By Frequency</see>
+    /// <br>Hash Table, String, Sorting, Heap(Priority Queue), Bucket Sort, Counting</br>
+    /// </summary>
+    internal class Problem0451
+    {
+        public string FrequencySort(string s)
+        {
+            var d = new Dictionary<char, IList<char>>();
+
+            foreach (var c in s)
+            {
+                if (d.TryGetValue(c, out var val))
+                {
+                    d[c].Add(c);
+                }
+                else
+                {
+                    d.Add(c, new List<char> { c });
+                }
+            }
+
+            return new string(d.Values.OrderByDescending(x => x.Count).SelectMany(x => x).ToArray());
+        }
+    }
+}
