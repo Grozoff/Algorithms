0933b436fe5d6d112bcf09ff1ba4b576f4f508b8
Sergei Grozov <grozov.sv@gmail.com>
2024-02-10 22:21:19 +0300

    leetcode problem 647

---
+++ LeetCodeProblems/Medium/Problem0647.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeProblems.Medium
+{
+    internal class Problem0647
+    {
+        public int CountSubstrings(string s)
+        {
+            int result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                result += CountSubstrings(s, i, i);
+                result += CountSubstrings(s, i, i + 1);
+            }
+
+            return result;
+        }
+
+        private int CountSubstrings(string s, int left, int right)
+        {
+            var result = 0;
+
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                result++;
+                left--;
+                right++;
+            }
+
+            return result;
+        }
+    }
+}
