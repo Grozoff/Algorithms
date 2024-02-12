cc3cfecd4fcb84d269be54f5fcd2d07a94718f89
Sergei Grozov <grozov.sv@gmail.com>
2024-02-12 11:44:16 +0300

    leetcode problem 169

---
+++ LeetCodeProblems/Easy/Problem0169.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/majority-element">Majority Element</see>
+    /// <br>Array, Hash Table, Divide and Conquer, Sorting, Counting</br>
+    /// </summary>
+    internal class Problem0169
+    {
+        public static int MajorityElement(int[] nums)
+        {
+            var dict = new Dictionary<int, int>();
+            foreach (var num in nums)
+            {
+                if (dict.TryGetValue(num, out var val))
+                {
+                    dict[num] = ++val;
+                }
+                else
+                {
+                    dict.Add(num, 1);
+                }
+            }
+            return dict.OrderBy(x => x.Value).Last().Key;
+
+            // cheating??
+            Array.Sort(nums);
+            return nums[nums.Length / 2];
+        }
+    }
+
+    public class Tests0169
+    {
+        [Test]
+        public void Test1()
+        {
+            var input = new[] { 6,5,5 };
+            var output = Problem0169.MajorityElement(input);
+
+            Assert.That(5, Is.EqualTo(output));
+        }
+    }
+}
