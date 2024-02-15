531bbec67859758d3af5c7d8eb7be287e937da11
Sergei Grozov <grozov.sv@gmail.com>
2024-02-15 10:06:38 +0300

    leetcode problem 2971

---
+++ LeetCodeProblems/Medium/Problem2971.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/find-polygon-with-the-largest-perimeter/'>Find Polygon With the Largest Perimeter</see>
+    /// <br>Array, Greedy, Sorting, Prefix Sum</br>
+    /// </summary>
+    internal class Problem2971
+    {
+        public long LargestPerimeter(int[] nums)
+        {
+            Array.Sort(nums);
+            long result = -1;
+            long total = 0;
+            foreach (var num in nums)
+            {
+                if (total > num)
+                {
+                    result = total + num;
+                }
+                total += num;
+            }
+
+            return result;
+        }
+    }
+}
