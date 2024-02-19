c74234c9f945ac1ebfa81249fe9e98850996d3ca
Sergei Grozov <grozov.sv@gmail.com>
2024-02-19 09:11:12 +0300

    leetcode problem 231

---
+++ LeetCodeProblems/Easy/Problem0231.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/power-of-two'>Power of Two</see>
+    /// <br>Math, Bit Manipulation, Recursion</br>
+    /// </summary>
+    internal class Problem0231
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            if (n == 0)
+                return false;
+
+            while (n != 1)
+            {
+                if (n % 2 != 0)
+                    return false;
+                n = n / 2;
+            }
+
+            return true;
+        }
+    }
+}
