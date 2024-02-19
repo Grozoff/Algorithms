bc07ea0314e7e1cb42d3f0193757545b2d679681
Sergei Grozov <grozov.sv@gmail.com>
2024-02-19 09:12:39 +0300

    leetcode problem 231

--- LeetCodeProblems/Easy/Problem0231.cs
+++ LeetCodeProblems/Easy/Problem0231.cs
@@ -15,10 +15,18 @@
             {
                 if (n % 2 != 0)
                     return false;
-                n = n / 2;
+                n /= 2;
             }
 
             return true;
+
+            // not my solution
+            if (n <= 0)
+            {
+                return false;
+            }
+
+            return (n & (n - 1)) == 0;
         }
     }
 }
