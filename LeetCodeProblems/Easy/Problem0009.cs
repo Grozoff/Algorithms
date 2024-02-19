ab5c35684b7a245b841f1395ad8bd74a6d307893
Sergei Grozov <grozov.sv@gmail.com>
2024-02-19 08:58:39 +0300

    leetcode problem 9

---
+++ LeetCodeProblems/Easy/Problem0009.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/palindrome-number'>Palindrome Number</see>
+    /// <br>Math</br>
+    /// </summary>
+    internal class Problem0009
+    {
+        public bool IsPalindrome(int x)
+        {
+            var s = x.ToString();
+            
+            return s.SequenceEqual(s.Reverse());
+        }
+    }
+}
