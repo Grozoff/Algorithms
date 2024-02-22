e10ac016f2a2a6d3affc4551a40a0c0845b410d1
Sergei Grozov <grozov.sv@gmail.com>
2024-02-22 10:25:56 +0300

    1768

---
+++ LeetCodeProblems/Easy/Problem1768.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/merge-strings-alternately'>Merge Strings Alternately</see>
+    /// <br>Two Pointers, String</br>
+    /// </summary>
+    internal class Problem1768
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < word1.Length || i < word2.Length; i++)
+            {
+                if (i < word1.Length)
+                {
+                    sb.Append(word1[i]);
+                }
+
+                if (i < word2.Length)
+                {
+                    sb.Append(word2[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
