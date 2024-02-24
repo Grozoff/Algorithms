b6c0829d1f33b497dab9c8d3ed360115290d9370
Sergei Grozov <grozov.sv@gmail.com>
2024-02-24 09:08:45 +0300

    392

---
+++ LeetCodeProblems/Easy/Problem0392.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/is-subsequence">Is Subsequence</see>
+    /// <br>Two Pointers, String, Dynamic Programming</br>
+    /// </summary>
+    internal class Problem0392
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            var sp = 0;
+            var tp = 0;
+            while (sp < s.Length && tp < t.Length)
+            {
+                if (s[sp] == t[tp])
+                {
+                    sp++;
+                    tp++;
+                }
+                else
+                {
+                    tp++;
+                }
+            }
+
+            return s.Length == sp;
+        }
+    }
+}
