ca698bf43110e559332512edbd9dcff78a3d9602
Sergei Grozov <grozov.sv@gmail.com>
2024-02-26 16:08:35 +0300

    100

---
+++ LeetCodeProblems/Easy/Problem0100.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/same-tree">Same Tree</see>
+    /// <br>Tree, Depth-First Search, Breadth-First Search, Binary Tree</br>
+    /// </summary>
+    internal class Problem0100
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null) return true;
+            if (p?.val != q?.val) return false;
+
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
