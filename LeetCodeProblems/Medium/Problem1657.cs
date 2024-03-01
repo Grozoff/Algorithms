7d2f1fda6c3a28848ab2443ec33efad1fc43524c
Sergei Grozov <grozov.sv@gmail.com>
2024-03-01 11:24:20 +0300

    1657

--- LeetCodeProblems/Medium/Problem1657.cs
+++ LeetCodeProblems/Medium/Problem1657.cs
@@ -31,6 +31,31 @@
             Array.Sort(charCount2);
 
             return charCount1.SequenceEqual(charCount2);
+
+            // not my solution
+            if (word1.Length != word2.Length) return false;
+
+            int[] frequency1 = new int[26];
+            int[] frequency2 = new int[26];
+
+            for (int index = 0; index < word1.Length; index++)
+            {
+                frequency1[word1[index] - 'a']++;
+                frequency2[word2[index] - 'a']++;
+            }
+
+            for (int index = 0; index < 26; index++)
+            {
+                if (frequency1[index] == 0 && frequency2[index] != 0)
+                    return false;
+                if (frequency1[index] != 0 && frequency2[index] == 0)
+                    return false;
+            }
+
+            Array.Sort(frequency1);
+            Array.Sort(frequency2);
+
+            return frequency1.SequenceEqual(frequency2);
         }
     }
 
