b57f041f769ad6397fa57bca2e6bc80d8cf2217f
Sergei Grozov <grozov.sv@gmail.com>
2024-03-01 11:08:02 +0300

    724

---
+++ LeetCodeProblems/Easy/Problem0724.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/find-pivot-index'>Find Pivot Index</see>
+    /// <br>Array, Prefix Sum</br>
+    /// </summary>
+    internal class Problem0724
+    {
+        public int PivotIndex(int[] nums)
+        {
+            var total = nums.Sum();
+            var leftSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var rightSum = total - nums[i] - leftSum;
+                if (leftSum == rightSum)
+                    return i;
+                leftSum += nums[i];
+            }
+            return -1;
+        }
+    }
+}
