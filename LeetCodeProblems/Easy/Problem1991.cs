82b4f0edf3c334ad7452fd528da2abc8cafe3ddf
Sergei Grozov <grozov.sv@gmail.com>
2024-03-01 11:09:45 +0300

    1991

---
+++ LeetCodeProblems/Easy/Problem1991.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/find-the-middle-index-in-array'>Find the Middle Index in Array</see>
+    /// <br>Array, Prefix Sum</br>
+    /// </summary>
+    internal class Problem1991
+    {
+        public int FindMiddleIndex(int[] nums)
+        {
+            var total = nums.Sum();
+            var leftSum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var rightSum = total - nums[i] - leftSum;
+                if (leftSum == rightSum)
+                    return i;
+                leftSum += nums[i];
+            }
+            return -1;
+        }
+    }
+}
