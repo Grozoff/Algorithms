c1f7660db4050d045f3a7d742987ab4d9fbf8d3d
Sergei Grozov <grozov.sv@gmail.com>
2024-03-01 10:16:02 +0300

    1732

---
+++ LeetCodeProblems/Easy/Problem1732.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/find-the-highest-altitude'>Find the Highest Altitude</see>
+    /// <br>Array, Prefix Sum</br>
+    /// </summary>
+    internal class Problem1732
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            var max = 0;
+            var prevAlt = 0;
+            foreach (var g in gain)
+            {
+                prevAlt += g;
+                max = Math.Max(prevAlt, max);
+            }
+            return max;
+        }
+    }
+}
