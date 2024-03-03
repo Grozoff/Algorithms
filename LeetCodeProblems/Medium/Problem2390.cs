1520bd6b3cec20d689c426764583c5d0a99f0925
Sergei Grozov <grozov.sv@gmail.com>
2024-03-03 22:30:33 +0300

    2390

---
+++ LeetCodeProblems/Medium/Problem2390.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/removing-stars-from-a-string">Removing Stars From a String</see>
+    /// <br>String, Stack, Simulation</br>
+    /// </summary>
+    internal class Problem2390
+    {
+        public string RemoveStars(string s)
+        {
+            var stack = new Stack<char>();
+            foreach (var ch in s)
+            {
+                if (ch == '*')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(ch);
+                }
+            }
+            return new string(stack.Reverse().ToArray());
+        }
+    }
+}
