c684ed9dfe29c29ab39e38377b4677cb524c57b0
Sergei Grozov <grozov.sv@gmail.com>
2024-03-04 16:44:46 +0300

    948

---
+++ LeetCodeProblems/Medium/Problem0948.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/removing-stars-from-a-string">Removing Stars From a String</see>
+    /// <br>Array, Two Pointers, Greedy, Sorting</br>
+    /// </summary>
+    internal class Problem0948
+    {
+        public int BagOfTokensScore(int[] tokens, int power)
+        {
+            Array.Sort(tokens);
+            var result = 0;
+            var score = 0;
+            var left = 0;
+            var right = tokens.Length - 1;
+
+            while (left <= right)
+            {
+                if (power >= tokens[left])
+                {
+                    power -= tokens[left];
+                    left++;
+                    score++;
+                    result = Math.Max(result, score);
+                }
+                else if (score > 0)
+                {
+                    power += tokens[right];
+                    right--;
+                    score--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
