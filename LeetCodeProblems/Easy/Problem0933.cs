784f495027f4f35d0cfac6e9156e6ab8a75cef6f
Sergei Grozov <grozov.sv@gmail.com>
2024-03-06 17:02:38 +0300

    933

---
+++ LeetCodeProblems/Easy/Problem0933.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/number-of-recent-calls">Number of Recent Calls</see>
+    /// <br>Design, Queue, Data Stream</br>
+    /// </summary>
+    internal class Problem0933
+    {
+    }
+
+    public class RecentCounter
+    {
+        Queue<int> q = new();
+        public RecentCounter()
+        {
+
+        }
+
+        public int Ping(int t)
+        {
+            q.Enqueue(t);
+
+            while (q.Peek() < t - 3000)
+            {
+                q.Dequeue();
+            }
+
+            return q.Count;
+        }
+    }
+}
