d54347f5af7c510ec847cba6daaefa2bcb38b165
Sergei Grozov <grozov.sv@gmail.com>
2024-03-11 14:09:12 +0300

    112

---
+++ LeetCodeProblems/Easy/Problem0112.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/path-sum">Path Sum</see>
+    /// <br>Tree, Depth-First Search, Breadth-First Search, Binary Tree</br>
+    /// </summary>
+    internal class Problem0112
+    {
+        public bool HasPathSum(TreeNode root, int targetSum)
+        {
+            return Dfs(root, targetSum, 0);
+        }
+
+        private bool Dfs(TreeNode node, int target, int current)
+        {
+            if (node == null) return false;
+
+            current += node.val;
+            if (node.left == null && node.right == null)
+                return current == target;
+
+            return Dfs(node.left, target, current) || Dfs(node.right, target, current);
+        }
+    }
+}
