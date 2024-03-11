9ed5afb0c4b63bfd3f5c5bd5477b0ac22f11931b
Sergei Grozov <grozov.sv@gmail.com>
2024-03-11 14:55:53 +0300

    113

---
+++ LeetCodeProblems/Medium/Problem0113.cs
@@ -0,0 +1,29 @@
+﻿using LeetCodeProblems.Easy;
+
+namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/path-sum-ii">Path Sum II</see>
+    /// <br>Backtracking, Tree, Depth-First Search, Binary Tree</br>
+    /// </summary>
+    internal class Problem0113
+    {
+        public IList<IList<int>> PathSum(TreeNode root, int targetSum)
+        {
+            var result = new List<IList<int>>();
+            Dfs(root, targetSum, [], result);
+            return result;
+        }
+
+        private void Dfs(TreeNode node, int target, List<int> path, List<IList<int>> result)
+        {
+            if (node == null) return;
+            path.Add(node.val);
+            if (node.left == null && node.right == null && target == node.val)
+                result.Add(new List<int>(path));
+            Dfs(node.left, target - node.val, path, result);
+            Dfs(node.right, target - node.val, path, result);
+            path.RemoveAt(path.Count - 1);
+        }
+    }
+}
