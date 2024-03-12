68479e08e01b1ba2a445dc83c473693f8ba7a566
Sergei Grozov <grozov.sv@gmail.com>
2024-03-12 13:39:48 +0300

    1372

---
+++ LeetCodeProblems/Medium/Problem1372.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeProblems.Easy;
+
+namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/longest-zigzag-path-in-a-binary-tree'>Longest ZigZag Path in a Binary Tree</see>
+    /// <br>Dynamic Programming, Tree, Depth-First Search, Binary Tree</br>
+    /// </summary>
+    internal class Problem1372
+    {
+        int result = 0;
+        public int LongestZigZag(TreeNode root)
+        {
+            ZigZag(root, true, 0);
+            ZigZag(root, false, 0);
+            return result;
+        }
+
+        private void ZigZag(TreeNode node, bool isRight, int path)
+        {
+            if (node == null) return;
+
+            result = Math.Max(result, path);
+            if (isRight)
+            {
+                ZigZag(node.left, true, 1);
+                ZigZag(node.right, false, path + 1);
+            }
+            else
+            {
+                ZigZag(node.left, true, path + 1);
+                ZigZag(node.right, false, 1);
+            }
+        }
+    }
+}
