bbf62c18de9007165ebbc62a4bbd9ff9d8ab210e
Sergei Grozov <grozov.sv@gmail.com>
2024-03-13 08:37:33 +0300

    2485

---
+++ LeetCodeProblems/Easy/Problem2485.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/find-the-pivot-integer'>Find the Pivot Integer</see>
+    /// <br>Math, Prefix Sum</br>
+    /// </summary>
+    internal class Problem2485
+    {
+        public int PivotInteger(int n)
+        {
+            // bruit force solution
+            var sum = 0;
+            for (int i = 1; i < n + 1; i++)
+            {
+                sum += i;
+            }
+            var temp = 0;
+            for (int i = 1; i < n + 1; i++)
+            {
+                sum -= i;
+                if (sum == temp)
+                {
+                    return i;
+                }
+                temp += i;
+            }
+
+            return -1;
+
+            // math solution
+            var sum1 = (n * (n + 1) / 2);
+            var pivot = (int)Math.Sqrt(sum);
+            // If pivot * pivot is equal to sum (pivot found) return pivot, else return -1
+            return pivot * pivot == sum ? pivot : -1;
+        }
+    }
+}
