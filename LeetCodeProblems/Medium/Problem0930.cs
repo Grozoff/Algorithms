9fa9f2801bbc8bb69b4e557d34cf78ef68c31ea7
Sergei Grozov <grozov.sv@gmail.com>
2024-03-14 09:22:39 +0300

    930

---
+++ LeetCodeProblems/Medium/Problem0930.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/binary-subarrays-with-sum'>Binary Subarrays With Sum</see>
+    /// <br>Array, Hash Table, Sliding Window, Prefix Sum</br>
+    /// </summary>
+    internal class Problem0930
+    {
+        public int NumSubarraysWithSum(int[] nums, int goal)
+        {
+            return Count(nums, goal) - Count(nums, goal - 1);
+        }
+
+        private int Count(int[] nums, int goal)
+        {
+            if (goal < 0) return 0;
+
+            var result = 0;
+            var left = 0;
+            var current = 0;
+
+            for (int right = 0; right < nums.Length; right++)
+            {
+                current += nums[right];
+                while (current > goal)
+                {
+                    current -= nums[left];
+                    left++;
+                }
+                result += (right - left + 1);
+            }
+
+            return result;
+        }
+    }
+}
