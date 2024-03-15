1856af8d765b5552b5b25a0d02d0d202c50baa7f
Sergei Grozov <grozov.sv@gmail.com>
2024-03-15 22:15:52 +0300

    700

---
+++ LeetCodeProblems/Easy/Problem0700.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/search-in-a-binary-search-tree'>Search in a Binary Search Tree</see>
+    /// <br>Tree, Binary Search Tree, Binary Tree</br>
+    /// </summary>
+    internal class Problem0700
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null || root.val == val) return root;
+
+            if (root.val < val)
+            {
+                return SearchBST(root.right, val);
+            }
+            else
+            {
+                return SearchBST(root.left, val);
+            }
+        }
+    }
+}
