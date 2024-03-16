d70042d749df05505240f64fbd76d1a344f86ada
Sergei Grozov <grozov.sv@gmail.com>
2024-03-16 11:44:19 +0300

    450

---
+++ LeetCodeProblems/Medium/Problem0450.cs
@@ -0,0 +1,37 @@
+﻿using LeetCodeProblems.Easy;
+
+namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href='https://leetcode.com/problems/delete-node-in-a-bst'>Delete Node in a BST</see>
+    /// <br>Tree, Binary Search Tree, Binary Tree</br>
+    /// </summary>
+    internal class Problem0450
+    {
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+            if (root == null) return root;
+            if (root.val < key)
+                root.right = DeleteNode(root.right, key);
+            else if (root.val > key)
+                root.left = DeleteNode(root.left, key);
+            else
+            {
+                if (root.left == null)
+                    return root.right;
+                if (root.right == null)
+                    return root.left;
+
+                var current = root.right;
+                while (current.left != null)
+                {
+                    current = current.left;
+                }
+                root.val = current.val;
+                root.right = DeleteNode(root.right, root.val);
+            }
+
+            return root;
+        }
+    }
+}
