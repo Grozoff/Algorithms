c4d27a8b300a3400c6beb188aab7f7f841dd3f8d
Sergei Grozov <grozov.sv@gmail.com>
2024-03-24 09:18:48 +0300

    287

---
+++ LeetCodeProblems/Medium/Problem0287.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/find-the-duplicate-number">Find the Duplicate Number</see>
+    /// <br>Array, Two Pointers, Binary Search, Bit Manipulation</br>
+    /// </summary>
+    internal class Problem0287
+    {
+        public int FindDuplicate(int[] nums)
+        {
+            var buffer = new bool[nums.Length];
+            foreach (var num in nums)
+            {
+                if (buffer[num - 1])
+                {
+                    return num;
+                }
+                buffer[num - 1] = true;
+            }
+            return -1;
+        }
+    }
+}
