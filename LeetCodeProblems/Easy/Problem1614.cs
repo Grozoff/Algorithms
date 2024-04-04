51dced8b5a64048830d101c68a110e9a827a2ce2
Sergei Grozov <grozov.sv@gmail.com>
2024-04-04 08:24:54 +0300

    1614

---
+++ LeetCodeProblems/Easy/Problem1614.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/">Maximum Nesting Depth of the Parentheses</see>
+    /// <br>String, Stack</br>
+    /// </summary>
+    internal class Problem1614
+    {
+        public int MaxDepth(string s)
+        {
+            var result = 0;
+            var max = 0;
+            foreach (var ch in s)
+            {
+                if (ch == '(')
+                {
+                    max++;
+                }
+                if (ch == ')')
+                {
+                    result = Math.Max(result, max);
+                    max--;
+                }
+            }
+            return result;
+        }
+    }
+}
