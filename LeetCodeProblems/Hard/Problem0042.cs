d087052f1dd8fa9f7b2125d4d0e5d59a4aebdf2f
Sergei Grozov <grozov.sv@gmail.com>
2024-04-13 21:05:41 +0300

    42

---
+++ LeetCodeProblems/Hard/Problem0042.cs
@@ -0,0 +1,46 @@
+﻿namespace LeetCodeProblems.Hard
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/trapping-rain-water/">Trapping Rain Water</see>
+    /// <br>Array, Two Pointers, Dynamic Programming, Stack, Monotonic Stack</br>
+    /// </summary>
+    internal class Problem0042
+    {
+        public int Trap(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int ans = 0;
+            int leftMax = 0;
+            int rightMax = 0;
+            while (left < right)
+            {
+                if (height[left] < height[right])
+                {
+                    if (height[left] >= leftMax)
+                    {
+                        leftMax = height[left];
+                    }
+                    else
+                    {
+                        ans += (leftMax - height[left]);
+                    }
+                    left++;
+                }
+                else
+                {
+                    if (height[right] >= rightMax)
+                    {
+                        rightMax = height[right];
+                    }
+                    else
+                    {
+                        ans += (rightMax - height[right]);
+                    }
+                    right--;
+                }
+            }
+            return ans;
+        }
+    }
+}
