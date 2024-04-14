78650949b55285912d61020afbaf16862ccd52b6
Sergei Grozov <grozov.sv@gmail.com>
2024-04-14 13:25:35 +0300

    404

---
+++ LeetCodeProblems/Easy/Problem0404.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeProblems.Easy
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/sum-of-left-leaves/">Sum of Left Leaves</see>
+    /// <br>Tree, Depth-First Search, Breadth-First Search, Binary Tree</br>
+    /// </summary>
+    internal class Problem0404
+    {
+        int result = 0;
+
+        public int SumOfLeftLeaves(TreeNode root, bool isLeft = false)
+        {
+            if (root == null) return 0;
+            if (root != null && isLeft && root.left == null && root.right == null)
+                result += root.val;
+            SumOfLeftLeaves(root.left, true);
+            SumOfLeftLeaves(root.right, false);
+            return result;
+        }
+    }
+}
