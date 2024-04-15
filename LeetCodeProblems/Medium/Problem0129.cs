fec70e9ec3b77484e04b1d6d6e4caf1df9ab5ee9
Sergei Grozov <grozov.sv@gmail.com>
2024-04-15 09:35:45 +0300

    129

---
+++ LeetCodeProblems/Medium/Problem0129.cs
@@ -0,0 +1,21 @@
+﻿using LeetCodeProblems.Easy;
+
+namespace LeetCodeProblems.Medium
+{
+    /// <summary>
+    /// <see href="https://leetcode.com/problems/sum-root-to-leaf-numbers/">Sum Root to Leaf Numbers</see>
+    /// <br>Tree, Depth-First Search, Binary Tree</br>
+    /// </summary>
+    internal class Problem0129
+    {
+        public int SumNumbers(TreeNode root, int currentSum = 0)
+        {
+            if (root == null) return 0;
+            var sum = root.val + (currentSum * 10);
+            if (root.left == null && root.right == null) return sum;
+            var l = SumNumbers(root.left, sum);
+            var r = SumNumbers(root.right, sum);
+            return l + r;
+        }
+    }
+}
